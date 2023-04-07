5236db7be83295ebb8b1c558ccbf96b0752c10f9
John Schieferle Uhlenbrock <johnsu@forgence.com>
2023-04-07 16:41:38 -0500

    Initial tests added

--- src/system_info.go
+++ src/system_info.go
@@ -13,14 +13,47 @@ import (
 )
 
 type SystemInfo struct {
-	HostInfo  *host.InfoStat  `json:"host_info"`
-	Processes []ProcessInfo   `json:"processes"`
-	CPUUsage  float64         `json:"cpu_usage"`
-	FileData  *FileScanResult `json:"file_data"`
+	HostInfo         *host.InfoStat  `json:"host_info"`
+	Processes        []ProcessInfo   `json:"processes"`
+	CPUUsage         float64         `json:"cpu_usage"`
+	FileData         *FileScanResult `json:"file_data"`
+	HostInfoProvider HostInfoProvider
+	CPUProvider      CPUProvider
+	ProcessProvider  ProcessProvider
+}
+
+type HostInfoProvider interface {
+	Info() (*host.InfoStat, error)
+}
+
+type DefaultHostInfoProvider struct{}
+
+func (p DefaultHostInfoProvider) Info() (*host.InfoStat, error) {
+	return host.Info()
+}
+
+type CPUProvider interface {
+	Percent(interval time.Duration, percpu bool) ([]float64, error)
+}
+
+type DefaultCPUProvider struct{}
+
+func (p DefaultCPUProvider) Percent(interval time.Duration, percpu bool) ([]float64, error) {
+	return cpu.Percent(interval, percpu)
+}
+
+type ProcessProvider interface {
+	Processes() ([]*process.Process, error)
+}
+
+type DefaultProcessProvider struct{}
+
+func (p DefaultProcessProvider) Processes() ([]*process.Process, error) {
+	return process.Processes()
 }
 
 func (sysInfo *SystemInfo) getHostInfo(debug bool, errorLog *log.Logger) {
-	hostInfo, err := host.Info()
+	hostInfo, err := sysInfo.HostInfoProvider.Info()
 	if err != nil {
 		if debug {
 			errorLog.Printf("Error getting host info: %v\n", err)
@@ -30,9 +63,8 @@ func (sysInfo *SystemInfo) getHostInfo(debug bool, errorLog *log.Logger) {
 	sysInfo.HostInfo = hostInfo
 }
 
-// system_info.go
 func (sysInfo *SystemInfo) getProcesses(debug bool, errorLog *log.Logger) {
-	processList, err := process.Processes()
+	processList, err := sysInfo.ProcessProvider.Processes()
 	if err != nil {
 		if debug {
 			errorLog.Printf("Error getting process list: %v\n", err)
@@ -46,14 +78,14 @@ func (sysInfo *SystemInfo) getProcesses(debug bool, errorLog *log.Logger) {
 		pid := proc.Pid
 		name, _ := proc.Name()
 		cmd, _ := proc.Cmdline()
-		createTime, err := proc.CreateTime() // Handle error from CreateTime
+		createTime, err := proc.CreateTime()
 		if err != nil {
 			if debug {
 				errorLog.Printf("Error getting process create time: %v\n", err)
 			}
 			continue
 		}
-		memoryInfo, err := proc.MemoryInfo() // Handle error from MemoryInfo
+		memoryInfo, err := proc.MemoryInfo()
 		if err != nil {
 			if debug {
 				errorLog.Printf("Error getting process memory info: %v\n", err)
@@ -76,7 +108,7 @@ func (sysInfo *SystemInfo) getProcesses(debug bool, errorLog *log.Logger) {
 }
 
 func (sysInfo *SystemInfo) getCPUUsage(debug bool, errorLog *log.Logger) {
-	cpuPercent, err := cpu.Percent(0, false)
+	cpuPercent, err := sysInfo.CPUProvider.Percent(0, false)
 	if err != nil {
 		if debug {
 			errorLog.Printf("Error getting CPU percent: %v\n", err)
@@ -89,7 +121,6 @@ func (sysInfo *SystemInfo) getCPUUsage(debug bool, errorLog *log.Logger) {
 	}
 }
 
-// system_info.go
 func (sysInfo *SystemInfo) scanFiles(ctx context.Context, startDir string, scanSubDirs, calculateHashes bool, excludePattern string, maxFileSize int64, fileTypes, hashAlgorithms []string, debug bool, errorLog *log.Logger) {
 	fileProcessor := make(chan FileData)
 	fileScanResult := &FileScanResult{
@@ -104,7 +135,6 @@ func (sysInfo *SystemInfo) scanFiles(ctx context.Context, startDir string, scanS
 		}
 	}()
 
-	// Updated call to traverseFiles
 	traverseFiles(ctx, startDir, fileProcessor, scanSubDirs, calculateHashes, excludePattern, maxFileSize, fileTypes, hashAlgorithms, debug, errorLog)
 	close(fileProcessor)
 
